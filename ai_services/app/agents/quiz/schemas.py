01a11cada9fe5bd27050f78dd8ec21884ec7cd35
AbdessamadTzn <abdessamadtouzani07@gmail.com>
2025-12-23 15:15:08 +0100

    fix: Correct type annotation, typo, and syntax in quiz schemas.

--- ai_services/app/agents/quiz/schemas.py
+++ ai_services/app/agents/quiz/schemas.py
@@ -2,8 +2,7 @@ from pydantic import BaseModel, Field
 from typing import List, Dict
 
 class QuizInput(BaseModel):
-
-    answers = Dict[str, str]
+    answers: Dict[str, str]
 
 class FormationDetails(BaseModel):
     name : str = Field(description="Le nom exacte de la formation recommandée issue du catalogue")
@@ -11,7 +10,7 @@ class FormationDetails(BaseModel):
 
 class RecommendationOutput(BaseModel):
     profil_analysis: str = Field(description="Une synthèse psychologique du profil utilisateur basee sur ses reponses (ex: 'Profil ambitieux mais bloqué par le stress')")
-    principal_program: FormationDetails = Field(escription="La formation signature prioritaire (ex: Prise de Parole)")
+    principal_program: FormationDetails = Field(description="La formation signature prioritaire (ex: Prise de Parole)")
     complementary_modules: List[FormationDetails] = Field(description="Liste de 1 ou 2 modules complémentaires pertinents")
-    motivation_message: str = Field(description="Une phrase de fin inspirante et bienveillante"
+    motivation_message: str = Field(description="Une phrase de fin inspirante et bienveillante")
         
